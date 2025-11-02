980d977a92b2da58faeb0ff8266191df0dc45093
si <si0534170928@gmail.com>
2025-11-03 01:03:10 +0200

    Initial commit of the new project

---
+++ lesson-2-htmlSerializer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+async Task<string> Load(string url)
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetAsync(url);
+    Console.WriteLine(response);
+    var html = await response.Content.ReadAsStringAsync();
+    return html;
+}
+var html = await Load("https://learn.malkabruk.co.il/practicode/projects/pract-2/#_2");
+var cleanHtml =new Regex("\\s").Replace(html," ");
+var htmlLines=new Regex("<(.*?)>").Split(cleanHtml).Where(s=>s.Length>0);
+var htmlElement = "<div id=\"my-id\" class=\"my-class-1 my-class-2\" width=\"100%\">text</div>";
+var attributes = new Regex(@"(\w+)=""(.*?)""").Matches(htmlElement);
+
+foreach (Match match in attributes)
+{
+    Console.WriteLine($"Attribute: {match.Groups[1].Value}, Value: {match.Groups[2].Value}");
+}
+
+
